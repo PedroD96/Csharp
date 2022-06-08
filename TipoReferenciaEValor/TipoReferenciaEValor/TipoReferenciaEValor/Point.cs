b127369fac3efb81d8ce1dcee0f327e48acce050
PedroD96 <pedro.duartebhmg96@gmail.com>
2022-06-07 22:01:11 -0300

    update fim do dia.

---
+++ TipoReferenciaEValor/TipoReferenciaEValor/TipoReferenciaEValor/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipoReferenciaEValor
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
--- TipoReferenciaEValor/TipoReferenciaEValor/TipoReferenciaEValor/Program.cs
+++ TipoReferenciaEValor/TipoReferenciaEValor/TipoReferenciaEValor/Program.cs
@@ -6,7 +6,14 @@ namespace TipoReferenciaEValor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            
+
         }
     }
 }
